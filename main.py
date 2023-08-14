4929012588984d7c29075b0d1ffa107326ff7548
Feraru Theodor Cristian <thc.feraru@gmail.com>
2023-08-14 14:30:27 +0300

    working beta version

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+import random
+import time
 
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
@@ -12,8 +14,8 @@ RED = [255, 0, 0]
 GREEN = [0, 255, 0]
 YELLOW = [255, 255, 0]
 
-BLOCK_SIZE = 15
-STEP = 15
+BLOCK_SIZE = 30
+STEP = BLOCK_SIZE
 
 EDGE_RIGHT = 600
 EDGE_LEFT = 120
@@ -32,38 +34,51 @@ pygame.display.set_caption("SNAKE")
 
 score = 0
 high_score = 0
+snake_length = 3
 
 play = False
 pause = False
-game_over = False
 
 class Segment(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, color, direction):
+    def __init__(self, pos_x, pos_y, direction):
         super().__init__()
         self.image = pygame.Surface([BLOCK_SIZE, BLOCK_SIZE])
-        self.image.fill(color)
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.direction = direction
-        self.rect.centerx = pos_x
-        self.rect.centery = pos_y
+        self.rect.x = pos_x
+        self.rect.y = pos_y
 
     def move_up(self):
-        self.rect.centery -= STEP
+        self.rect.y -= STEP
 
     def move_down(self):
-        self.rect.centery += STEP
+        self.rect.y += STEP
 
     def move_left(self):
-        self.rect.centerx -= STEP
+        self.rect.x -= STEP
     
     def move_right(self):
-        self.rect.centerx += STEP
+        self.rect.x += STEP
 
 class Apple():
-    def __init__(self, pos_x, pos_y, exist):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.exist = exist
+    def __init__(self):
+        super().__init__()
+        self.exist = False
+        self.image = pygame.Surface([BLOCK_SIZE, BLOCK_SIZE])
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+    
+    def generate(self):
+        while self.exist == False:
+            self.rect.x = random.randint(EDGE_LEFT // BLOCK_SIZE, EDGE_RIGHT // BLOCK_SIZE) * BLOCK_SIZE
+            self.rect.y = random.randint(EDGE_TOP // BLOCK_SIZE, EDGE_BOTTOM // BLOCK_SIZE) * BLOCK_SIZE
+
+            for segment in snake:
+                if self.rect.center != segment.rect.center and check_out_of_bounds(self) == False:
+                    self.exist = True
+
+
 
 def move_snake(snake):
     next_dir = ""
@@ -90,14 +105,28 @@ def move_snake(snake):
         if (next_dir != ""):
             segment.direction = next_dir
 
-def check_out_of_bounds(snake_head:Segment):
-    if snake_head.rect.centerx < EDGE_LEFT:
+def snake_add(snake:pygame.sprite.Group):
+    snake_tail:Segment = snake.sprites()[snake_length-1]
+
+    if snake_tail.direction == UP:
+        new_segment = Segment(snake_tail.rect.x, snake_tail.rect.y + BLOCK_SIZE, UP)
+    if snake_tail.direction == DOWN:
+        new_segment = Segment(snake_tail.rect.x, snake_tail.rect.y - BLOCK_SIZE, DOWN)
+    if snake_tail.direction == LEFT:
+        new_segment = Segment(snake_tail.rect.x + BLOCK_SIZE, snake_tail.rect.y, LEFT)
+    if snake_tail.direction == RIGHT:
+        new_segment = Segment(snake_tail.rect.x - BLOCK_SIZE, snake_tail.rect.y, RIGHT)
+
+    snake.add(new_segment)
+
+def check_out_of_bounds(object):
+    if object.rect.x < EDGE_LEFT:
         return True
-    if snake_head.rect.centerx > EDGE_RIGHT:
+    if object.rect.x > EDGE_RIGHT - BLOCK_SIZE:
         return True
-    if snake_head.rect.centery < EDGE_TOP:
+    if object.rect.y < EDGE_TOP:
         return True
-    if snake_head.rect.centery > EDGE_BOTTOM:
+    if object.rect.y > EDGE_BOTTOM - BLOCK_SIZE:
         return True
     return False
     
@@ -111,20 +140,51 @@ def check_self_collision(snake:pygame.sprite.Group):
         skip_snake_head = False
     return False
 
+def check_snake_apple_collision(snake:pygame.sprite.Group, apple:Apple):
+    for segment in snake:
+        if apple.rect.center == segment.rect.center:
+            return True
+    return False
+
+def game_over():
+    global snake, score, high_score, snake_length, play
+
+    play = False
+
+    if score > high_score: high_score = score
+    score = 0
+
+    snake_length = 3
+
+    time.sleep(0.5)
+
+    font = pygame.font.SysFont('Bahnschrift', 32, False, False)
+    text = font.render("GAME OVER", True, RED)
+    viewport.blit(text, [EDGE_RIGHT / 2 - 20, EDGE_BOTTOM / 2 - 20])
+
+    pygame.display.flip()
+
+    time.sleep(2)
+
+    snake.empty()
+    for i in range(0, snake_length):   
+        new_segment = Segment(360- i*BLOCK_SIZE, 360, RIGHT)
+        snake.add(new_segment)
 
 
 running = True
 UPDATE = pygame.USEREVENT
 pygame.time.set_timer(UPDATE, 250)
 clock = pygame.time.Clock()
-input = RIGHT
 
 snake = pygame.sprite.Group()
-for i in range(0, 7):   
-    segment = Segment(360- i*BLOCK_SIZE, 360, WHITE, RIGHT)
-    snake.add(segment)
-snake_length = 7
+for i in range(0, snake_length):   
+    new_segment = Segment(360- i*BLOCK_SIZE, 360, RIGHT)
+    snake.add(new_segment)
 
+apple = Apple()
+
+input = RIGHT
 
 
 while running:
@@ -133,9 +193,18 @@ while running:
     text = font.render("S N A K E" , False, GREEN)
     viewport.blit(text, [EDGE_RIGHT / 2 - 55, 20])
 
-    arena = pygame.draw.rect(viewport, WHITE, (EDGE_LEFT, EDGE_TOP, 480, 480), 1)
+    font = pygame.font.SysFont('Bahnschrift', 24, False, False)
+    text = font.render(f"SCORE: {score}" , False, WHITE)
+    viewport.blit(text, [EDGE_LEFT, EDGE_TOP - 25])
     
-    snake.draw(viewport)
+    text = font.render(f"HIGH: {high_score}" , False, WHITE)
+    viewport.blit(text, [EDGE_RIGHT - 85, EDGE_TOP - 25])
+
+    font = pygame.font.SysFont('Bahnschrift', 20, False, False)
+    text = font.render("press SPACE to pause" , False, WHITE)
+    viewport.blit(text, [EDGE_RIGHT / 2 - 30, EDGE_BOTTOM + 20])
+
+    arena = pygame.draw.rect(viewport, WHITE, (EDGE_LEFT, EDGE_TOP, 480, 480), 1)
 
     snake_direction = snake.sprites()[0].direction
 
@@ -143,6 +212,17 @@ while running:
     
     if key[pygame.K_RETURN] and play == False:
         play = True
+    
+    if key[pygame.K_SPACE]:
+        if play:
+            play = False
+        else:
+            play = True
+
+        if pause:
+            pause = False
+        else:
+            pause = True
     
     if play:
         if key[pygame.K_UP] and snake_direction != DOWN:
@@ -154,16 +234,39 @@ while running:
         if key[pygame.K_RIGHT] and snake_direction != LEFT:
             input = RIGHT
 
+        snake.draw(viewport)
+
+        if check_snake_apple_collision(snake, apple) == True:
+            apple.exist = False
+            score += 1
+            snake_add(snake)
+            snake_length += 1
+
+        if apple.exist == False:
+            apple.generate()
+            
+        viewport.blit(apple.image, [apple.rect.x, apple.rect.y])
+
+    elif (pause):
+        font = pygame.font.SysFont('Bahnschrift', 32, False, False)
+        text = font.render("PAUSED", True, YELLOW)
+        viewport.blit(text, [EDGE_RIGHT / 2 + 5, EDGE_BOTTOM / 2 - 20])
+
+    else:
+        font = pygame.font.SysFont('Bahnschrift', 20, False, False)
+        text = font.render("press ENTER to start" , False, WHITE)
+        viewport.blit(text, [EDGE_RIGHT / 2 - 30, EDGE_BOTTOM / 2 + 20])
+
     snake.sprites()[0].direction = input
 
     for event in pygame.event.get():
         
         if event.type == UPDATE and play:
             move_snake(snake)
-            if check_out_of_bounds(snake.sprites()[0]): game_over = True
-            if check_self_collision(snake): game_over = True
+            if check_out_of_bounds(snake.sprites()[0]): game_over()
+            if check_self_collision(snake): game_over()
         
-        if event.type == pygame.QUIT or game_over:
+        if event.type == pygame.QUIT or key[pygame.K_ESCAPE]:
             pygame.quit()
             sys.exit()
 
